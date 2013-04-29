52c78df323e0545e4fd8b4cfdf597b740e96efac
Sebastian Vogel <s.vogel@gentics.com>
2013-04-29 09:19:12 +0200

    changes from sunday

--- js/controller.js
+++ js/controller.js
@@ -3,5 +3,13 @@
 /* Controllers */
 function MainCtrl($scope) {
 	$scope.active = true;
+	$scope.alohaFormats = ['strong', 'em'];
+	$scope.toggleAloha = function () {
+		$scope.active = $scope.active ? false : true;
+	};
+	$scope.$watch('active', function () {
+		$scope.classActive = $scope.active ? 'active' : '';
+	});
+
 }
 
