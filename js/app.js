91954dca4718e95cf16e52c8a0473fe58082d0fa
Sebastian Vogel <s.vogel@gentics.com>
2013-06-06 15:19:10 +0200

    added demos

--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@
 angular.module('alohaShowcase', ['aloha', 'couchDb']).
 config(['$routeProvider', function($routeProvider) {
 	$routeProvider
-	.when('/', {controller: MainCtrl})
-	.otherwise({redirectTo: '/'});
+	.when('/Demo/1', {controller: Demo1Ctrl, templateUrl: 'partials/Demo1.html'})
+	.when('/Demo/2', {controller: Demo2Ctrl, templateUrl: 'partials/Demo2.html'})
+	.when('/Demo/3', {controller: Demo3Ctrl, templateUrl: 'partials/Demo3.html'})
+	.when('/Demo/Aloha', {controller: AlohaCtrl, templateUrl: 'partials/Aloha.html'})
+	.otherwise({redirectTo: '/Demo/1'});
 }]);
--- js/controller.js
+++ js/controller.js
@@ -1,23 +1,55 @@
 'use strict';
 
+function Demo1Ctrl($scope) {
+	$scope.curly= {
+		open: "{{",
+		close: "}}"
+	};
+	$scope.list = [
+		{ content: "ListItem 1"},
+		{ content: "ListItem 2"},
+		{ content: "ListItem 3"},
+		{ content: "ListItem 4"}
+	];
+}
+
+function Demo2Ctrl($scope) {
+	$scope.curly= {
+		open: "{{",
+		close: "}}"
+	};
+
+	$scope.dataBinding = "Awesome 2-way data binding";
+}
+
+function Demo3Ctrl($scope, CouchDbService) {
+	$scope.curly= {
+		open: "{{",
+		close: "}}"
+	};
+	$scope.couch = CouchDbService.query({_id: "index"});
+}
+
+
 /* Controllers */
-function MainCtrl($scope, CouchDbService) {
-	$scope.active = true;
-	$scope.alohaConfig = {
+function AlohaCtrl($scope, CouchDbService) {
+	$scope.active = false;
+	$scope.alohaConfigHead = {
 		formats: ['strong', 'em']
 	};
-	$scope.indexContent = CouchDbService.query({_id: "index"});
+	$scope.alohaConfigBody = {
+		formats: ['strong', 'em', 'h3', 'p']
+	};
+	$scope.couch = CouchDbService.query({_id: "index"});
 	$scope.toggleAloha = function () {
 		$scope.active = $scope.active ? false : true;
 	};
 	$scope.save = function () {
-		$scope.indexContent.$save(function () {
-			$scope.indexContent = CouchDbService.query({_id: "index"});
+		$scope.couch.$save(function () {
+			$scope.couch = CouchDbService.query({_id: "index"});
 		});
 	};
 	$scope.$watch('active', function () {
 		$scope.classActive = $scope.active ? 'active' : '';
 	});
-
 }
-
